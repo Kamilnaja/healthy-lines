7369ebfa23a1bda4a668fcb550dcaf8b48adc15d
kamilnaja <kamilnaja@gmail.com>
2022-06-25 16:13:36 +0200

    Changing main element

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const HelloWorld());
+void main() => runApp(const MyApp());
 
-class HelloWorld extends StatelessWidget {
-  const HelloWorld({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'First App',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Home Page'),
-            ),
-            body: const Center(
-              child: Text('Hello World'),
-            )));
+      title: 'First App',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: MyHomePage(title: 'MyHomePage'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, this.title = ''}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: const Center(
+          child: Text(
+              'Take place in our challenges, or your (or you) pet gonna die'),
+        ));
   }
 }

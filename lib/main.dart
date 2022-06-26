a984c1942787d559bdd3ff2b483b1b04c421e670
kamilnaja <kamilnaja@gmail.com>
2022-06-26 18:09:46 +0200

    Adding new package

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First App',
       theme: ThemeData(primarySwatch: Colors.amber),
-      home: MyHomePage(title: 'MyHomePage'),
+      home: const MyHomePage(title: 'MyHomePage'),
     );
   }
 }
@@ -25,9 +25,35 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Home Page'),
         ),
-        body: const Center(
-          child: Text(
-              'Take place in our challenges, or your (or you) pet gonna die'),
-        ));
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+              BoxCustomComponent(name: "Lorem"),
+              SizedBox(height: 8.0),
+              BoxCustomComponent(name: "Ipsum"),
+              SizedBox(height: 8.0),
+              BoxCustomComponent(name: "Dolor")
+            ])),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () => print('hello world'),
+            tooltip: 'Increment Counter',
+            child: const Icon(Icons.add)));
+  }
+}
+
+class BoxCustomComponent extends StatelessWidget {
+  final String name;
+
+  const BoxCustomComponent({Key? key, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(color: Colors.lightBlueAccent),
+      child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Text(name)),
+    );
   }
 }
